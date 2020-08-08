cd6cf42eebdc34550ccb6f7b3f54d6090cbdcabc
Lennart <lennart@lnrt.de>
2020-08-08 15:41:23 +0200

    yaddayadda

--- config.def.h
+++ config.def.h
@@ -29,7 +29,9 @@ static const char *colors[][3]           = {
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
-static const Rule rules[] = {0};
+static const Rule rules[] = {
+	{ "st-256color",	NULL,       "tmux",       1  << 2,            0,           -1 },
+};
 
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
